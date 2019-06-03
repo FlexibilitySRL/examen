49471dcf3c11df604dbfbb257861d9019d90baeb
TiagoQuinteros <38963262+TiagoQuinteros@users.noreply.github.com>
2019-06-03 01:56:30 -0300

    agregue la documentacion y una funcion
    agregue la documentacion

--- src/main/java/ar/com/flexibility/examen/app/rest/VentasController.java
+++ src/main/java/ar/com/flexibility/examen/app/rest/VentasController.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,18 @@ public class VentasController {
 			return null;
 		}
 	}
-	
+	@GetMapping(value = "/{id}")
+		public Ventas Buscarventa(@PathVariable("id") Integer id) {
+		logger.info("Buscando Venta");
+		try {
+			logger.info("Venta encontrada");
+			return repo.findOne(id);			
+		}catch(Exception e) {
+			logger.info("Error al encontrar la venta");
+			return null;
+		}
+		
+	}
 	@PostMapping
 	public void alta(@RequestBody Ventas venta) {
 		repo.save(venta);
