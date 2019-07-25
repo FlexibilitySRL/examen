e1ba96df11d4195ff34396bab2801ea795c1f696
Nahuel Burgos Fuentes <burgosfuentesn@gmail.com>
2019-07-25 11:33:25 -0300

    creo el repository de Product

---
+++ src/main/java/ar/com/flexibility/examen/domain/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package ar.com.flexibility.examen.domain.repository;
+
+import ar.com.flexibility.examen.domain.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product,Long> {
+}
