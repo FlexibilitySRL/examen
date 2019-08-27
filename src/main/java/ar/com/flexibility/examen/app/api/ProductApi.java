de9dd6bd3473d07085aa72fc3e51f26568dbf60d
Felipe <felipebanfi84@gmail.com>
2019-08-27 16:50:30 -0300

    Add files via upload

--- src/main/java/ar/com/flexibility/examen/app/api/ProductApi.java
+++ src/main/java/ar/com/flexibility/examen/app/api/ProductApi.java
@@ -1,6 +1,5 @@
 package ar.com.flexibility.examen.app.api;
 
-import ar.com.flexibility.examen.domain.model.Product;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
@@ -16,7 +15,7 @@ public class ProductApi {
     @JsonProperty
     private String description;
     @JsonProperty
-    private BigDecimal price;
+    private double price;
 
     @JsonProperty
     private Integer stock;
@@ -33,33 +32,12 @@ public class ProductApi {
         this.description = description;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
     public void setStock(Integer stock) {
         this.stock = stock;
-    }
-
-    public Product toEntity()
-    {
-        Product product = new Product();
-        product.setId(id);
-        product.setName(name);
-        product.setDescription(description);
-        product.setPrice(price);
-        product.setStock(stock);
-        return product;
-    }
-
-    public static ProductApi toApi(Product product) {
-        final ProductApi productApi = new ProductApi();
-        if(product != null) {
-            productApi.setId(product.getId());
-            productApi.setName(product.getName());
-            productApi.setDescription(product.getDescription());
-        }
-        return productApi;
     }
 
     public Long getId() { return id;  }
@@ -68,7 +46,7 @@ public class ProductApi {
 
     public String getDescription() { return description; }
 
-    public BigDecimal getPrice() { return price; }
+    public double getPrice() { return price; }
 
     public Integer getStock() { return stock; }
 }
