6d2a0a6082b29fc664cc9cc16f92a62e9f34bf76
Felipe <felipebanfi84@gmail.com>
2019-08-27 16:46:24 -0300

    Add files via upload
    comentarios

--- src/main/java/ar/com/flexibility/examen/domain/service/ProductService.java
+++ src/main/java/ar/com/flexibility/examen/domain/service/ProductService.java
@@ -8,9 +8,36 @@ import java.util.Optional;
 
 public interface ProductService {
 
-    public Product create(Product product);
-    public Product update(Product product);
-    public Product findById(Long id);
-    public List<Product> findAll();
-    public void deleteById(Long id);
+    /**
+     * Crear un producto
+     * @param product a crear
+     * @return product creado
+     */
+	public Product create(Product product);
+    
+	/**
+	 * Actualiza un producto
+	 * @param product a actualizar
+	 * @return Product actualizado
+	 */
+	public Product update(Product product);
+    
+	/**
+     * Retorna un producto en base al id
+     * @param id
+     * @return Product
+     */
+	public Product findById(Long id);
+    
+	/**
+     * Retorna todos los productos
+     * @return List<Product>
+     */
+	public List<Product> findAll();
+    
+	/**
+	 * Borra un producto en base al id
+	 * @param id
+	 */
+	public void deleteById(Long id);
 }
