77b2874c43aac1628d24fe6de9069c60f5656d53
Felipe <felipebanfi84@gmail.com>
2019-08-28 16:31:30 -0300

    Add files via upload

--- src/main/java/ar/com/flexibility/examen/domain/repository/ProductRepository.java
+++ src/main/java/ar/com/flexibility/examen/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package ar.com.flexibility.examen.domain.repository;
 
 import ar.com.flexibility.examen.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long>{
 }
