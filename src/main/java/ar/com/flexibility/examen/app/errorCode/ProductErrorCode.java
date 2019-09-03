fbcc471cf2659903f3ad057a8b0e14335d814d4b
david.rojas <david.rojas@tecso.coop>
2019-09-03 18:09:01 -0300

    Agregado Error codes para Product y Transaction

---
+++ src/main/java/ar/com/flexibility/examen/app/errorCode/ProductErrorCode.java
@@ -0,0 +1,8 @@
+package ar.com.flexibility.examen.app.errorCode;
+
+public enum ProductErrorCode {
+	PRODUCT_NOT_FOUND,
+	PRODUCT_INVALID_NAME,
+	PRODUCT_INVALID_CODE
+
+}
---
+++ src/main/java/ar/com/flexibility/examen/app/errorCode/TransactionErrorCode.java
@@ -0,0 +1,8 @@
+package ar.com.flexibility.examen.app.errorCode;
+
+public enum TransactionErrorCode {
+	TRANSACTION_NOT_FOUND,
+	TRANSACTION_INVALID_CUSTOMER_ID,
+	TRANSACTION_INVALID_PRODUCT_ID,
+	TRANSACTION_INVALID_AMOUNT
+}
