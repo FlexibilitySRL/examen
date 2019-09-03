91be178acf73df4342f74a3011939de495bddd7b
david.rojas <david.rojas@tecso.coop>
2019-09-03 15:03:08 -0300

    Refactor de entity Transaction y agregado status

--- src/main/java/ar/com/flexibility/examen/domain/model/Transaction.java
+++ src/main/java/ar/com/flexibility/examen/domain/model/Transaction.java
@@ -1,12 +1,19 @@
 package ar.com.flexibility.examen.domain.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import ar.com.flexibility.examen.domain.base.BaseEntity;
 
@@ -21,13 +28,26 @@ public class Transaction extends BaseEntity {
 	@JoinColumn(name = "customer_id", referencedColumnName = "id")
 	private Customer customer;
 
+	@Column(name = "status")
+	private TransactionStatus status;
+
+	@Column(name = "amount")
+	private BigDecimal amount;
+
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "creation_datetime", updatable = false)
+	private Date creationDateTime;
+
 	public Transaction() {
 	}
 
-	public Transaction(List<Product> products, Customer customer) {
+	public Transaction(List<Product> products, Customer customer, TransactionStatus status, BigDecimal amount) {
 		super();
 		this.products = products;
 		this.customer = customer;
+		this.status = status;
+		this.amount = amount;
 	}
 
 	public List<Product> getProducts() {
@@ -44,6 +64,30 @@ public class Transaction extends BaseEntity {
 
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
+	}
+
+	public TransactionStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TransactionStatus status) {
+		this.status = status;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public Date getCreationDateTime() {
+		return creationDateTime;
+	}
+
+	public void setCreationDateTime(Date creationDateTime) {
+		this.creationDateTime = creationDateTime;
 	}
 
 }
---
+++ src/main/java/ar/com/flexibility/examen/domain/model/TransactionStatus.java
@@ -0,0 +1,7 @@
+package ar.com.flexibility.examen.domain.model;
+
+public enum TransactionStatus {
+	
+	APPROVED, PENDING, DISAPPROVED 
+	
+}
