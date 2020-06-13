9d16329510e938cfaf357cdc1b53e9cbe676488a
Flavio Ortellao <fortellao@itx.com>
2020-06-13 19:27:09 -0300

    Product Service unit tests

--- src/test/java/ar/com/flexibility/examen/domain/service/ProductServiceTest.java
+++ src/test/java/ar/com/flexibility/examen/domain/service/ProductServiceTest.java
@@ -1,65 +1,48 @@
 package ar.com.flexibility.examen.domain.service;
 
 import ar.com.flexibility.examen.domain.model.Product;
+import ar.com.flexibility.examen.domain.repo.ProductRepository;
+import ar.com.flexibility.examen.domain.service.impl.ProductServiceImpl;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+
+@RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTest {
     
-    @Autowired
-    ProductService svc;
+    @Mock
+    ProductRepository repo;
     
+    @InjectMocks
+    ProductServiceImpl svc;
+        
     @Test
     public void createTest() {
+        
+        Product productNew = new Product();
+        productNew.setName("Product Name");
         Product product = new Product();
         product.setName("Product Name");
-        Product productRes = svc.create(product);
-        Assert.assertEquals(product.getName(), productRes.getName());
+        product.setId(1L);
+
+        when(repo.save(productNew)).thenReturn(product);
+
+        Product productRes = svc.create(productNew);
+        Assert.assertEquals(productNew.getName(), productRes.getName());
     }
     
     @Test
     public void readTest() {
-        String productName = "ProductName 1";
         Product product = new Product();
-        product.setName(productName);
-        product = svc.create(product);
-
-        Product productRead = svc.read(product.getId());
-        Assert.assertEquals(productName, productRead.getName());
-    }
-
-    @Test
-    public void updateTest() {
-        String maskname = "ProductName 2";
-        Product product = new Product();
-        product.setName(maskname);
-        product = svc.create(product);
-
-        String productName = "Updated Product Name";
-        Long id = product.getId();
-        Product updProduct = new Product();
-        updProduct.setId(id);
-        updProduct.setName(productName);
-        updProduct = svc.update(updProduct);
-        Assert.assertEquals("A new object were created", product.getId(), updProduct.getId());
-        Assert.assertEquals("Product Name wasn't updated", productName, svc.read(id).getName());
-    }
-
-    @Test
-    public void deleteTest() {
-        String maskname = "maskname 3";
-        Product product = new Product();
-        product.setName(maskname);
-        product = svc.create(product);
-
-        svc.delete(product.getId());
-        Assert.assertNull(svc.read(product.getId()));
-    }
-    
+        product.setId(1L);
+        when(repo.findOne(1L)).thenReturn(product);
+        
+        Product productRead = svc.read(1L);
+        Assert.assertEquals(Long.valueOf(1), productRead.getId());
+    }    
 }
