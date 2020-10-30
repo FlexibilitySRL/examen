3eb9af431f9ebfc8485648c06290b3fa35be78b4
Martín Cortina <micortina@teco.com.ar>
2020-10-29 23:35:36 -0300

    Agregando mapeos para las entidades faltantes

--- src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
+++ src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
@@ -1,12 +1,13 @@
 package ar.com.plug.examen.domain.mapper;
 
-import javax.validation.Valid;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import ar.com.plug.examen.domain.model.Customer;
 import ar.com.plug.examen.domain.model.Product;
+import ar.com.plug.examen.domain.model.PurchaseTransaction;
+import ar.com.plug.examen.domain.model.Vendor;
+
 
 @Mapper(componentModel = "spring")
 public interface DTOMapper {
@@ -14,9 +15,19 @@ public interface DTOMapper {
 	DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
 	public Product from(ar.com.plug.generated.model.Product productDTO);
+
 	public ar.com.plug.generated.model.Product from(Product productEntity);
 
 	public Customer from(ar.com.plug.generated.model.Customer customerDTO);
+
 	public ar.com.plug.generated.model.Customer from(Customer customerEntity);
+
+	public Vendor from(ar.com.plug.generated.model.Vendor vendorDTO);
+
+	public ar.com.plug.generated.model.Vendor from(Vendor vendorEntity);
+
+	public PurchaseTransaction from(ar.com.plug.generated.model.PurchaseTransaction purchaseTransactionDTO);
+
+	public ar.com.plug.generated.model.PurchaseTransaction from(PurchaseTransaction purchaseTransactionEntity);
 
 }
