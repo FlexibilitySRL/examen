c303f8fbd5f72d225e311be51ea5e810f7ab27b9
Martín Cortina <micortina@teco.com.ar>
2020-10-29 23:29:24 -0300

    Agregando mapeos adicionales para Customer

--- src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
+++ src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
@@ -1,8 +1,11 @@
 package ar.com.plug.examen.domain.mapper;
 
+import javax.validation.Valid;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import ar.com.plug.examen.domain.model.Customer;
 import ar.com.plug.examen.domain.model.Product;
 
 @Mapper(componentModel = "spring")
@@ -11,7 +14,9 @@ public interface DTOMapper {
 	DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
 	public Product from(ar.com.plug.generated.model.Product productDTO);
-
 	public ar.com.plug.generated.model.Product from(Product productEntity);
+
+	public Customer from(ar.com.plug.generated.model.Customer customerDTO);
+	public ar.com.plug.generated.model.Customer from(Customer customerEntity);
 
 }
