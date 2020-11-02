c03f51ea345c590210b71eae9755c9ec4dae9d6f
Martín Cortina <micortina@teco.com.ar>
2020-11-02 12:27:45 -0300

    Se agregan operaciones faltantes CRUD a la entidad Customer

--- src/main/java/ar/com/plug/examen/app/rest/CustomersApiController.java
+++ src/main/java/ar/com/plug/examen/app/rest/CustomersApiController.java
@@ -1,6 +1,8 @@
 package ar.com.plug.examen.app.rest;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -15,6 +17,7 @@ import ar.com.plug.examen.domain.model.Customer;
 import ar.com.plug.examen.domain.service.impl.CustomerServiceImpl;
 import ar.com.plug.generated.api.CustomersApi;
 import lombok.extern.slf4j.Slf4j;
+
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-10-29T10:08:36.293-03:00[America/Argentina/Buenos_Aires]")
 
 @Slf4j
@@ -22,34 +25,104 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("${openapi.paymentManagement.base-path:}")
 public class CustomersApiController implements CustomersApi {
 
-    private final NativeWebRequest request;
+	private final NativeWebRequest request;
 
 	private CustomerServiceImpl customerService;
 
-	
 	private DTOMapper dtoMapper = DTOMapper.INSTANCE;
 
-	
-    @org.springframework.beans.factory.annotation.Autowired
-    public CustomersApiController(NativeWebRequest request) {
-        this.request = request;
-    }
+	@org.springframework.beans.factory.annotation.Autowired
+	public CustomersApiController(NativeWebRequest request, CustomerServiceImpl customerService) {
+		this.request = request;
+		this.customerService = customerService;
+	}
 
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
+	@Override
+	public Optional<NativeWebRequest> getRequest() {
+		return Optional.ofNullable(request);
+	}
 
-    
-    @Override
-    public ResponseEntity<Void> createCustomer(@Valid ar.com.plug.generated.model.Customer customerDTO) {
+	/**
+	 * Creates the customer.
+	 *
+	 * @param customerDTO the customer DTO
+	 * @return the response entity
+	 */
+	@Override
+	public ResponseEntity<ar.com.plug.generated.model.Customer> createCustomer(
+			ar.com.plug.generated.model.@Valid Customer customerDTO) {
+		log.info("Se recibe una solicitud de creación de cliente {}", customerDTO);
 
-    	log.info("Se recibe una solicitud de creación de cliente {}", customerDTO);
+		Customer customerEntity = dtoMapper.from(customerDTO);
 
-    	Customer customerEntity = dtoMapper.from(customerDTO);
-    	
-    	customerEntity = customerService.createCustomer(customerEntity);
-    	
-		return new ResponseEntity<Void>(HttpStatus.CREATED);
-    }
+		customerEntity = customerService.createCustomer(customerEntity);
+		customerDTO = dtoMapper.from(customerEntity);
+
+		return ResponseEntity.ok().body(customerDTO);
+	}
+
+	/**
+	 * Delete customer.
+	 *
+	 * @param customerId the customer id
+	 * @return the response entity
+	 */
+	@Override
+	public ResponseEntity<Void> deleteCustomer(Integer customerId) {
+		customerService.deleteCustomer(customerId.longValue());
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	/**
+	 * Update customer.
+	 *
+	 * @param customerId  the customer id
+	 * @param customerDTO the customer DTO
+	 * @return the response entity
+	 */
+	@Override
+	public ResponseEntity<ar.com.plug.generated.model.Customer> updateCustomer(Integer customerId,
+			ar.com.plug.generated.model.@Valid Customer customerDTO) {
+		log.info("Se recibe una solicitud de actualización de cliente {}", customerDTO);
+
+		Customer customerEntity = dtoMapper.from(customerDTO);
+
+		customerEntity = customerService.updateCustomer(customerId.longValue(), customerEntity);
+		customerDTO = dtoMapper.from(customerEntity);
+
+		return ResponseEntity.ok().body(customerDTO);
+	}
+
+	/**
+	 * Gets the customer.
+	 *
+	 * @param customerId the customer id
+	 * @return the customer
+	 */
+	@Override
+	public ResponseEntity<ar.com.plug.generated.model.Customer> getCustomer(Integer customerId) {
+		return ResponseEntity.ok().body(dtoMapper.from(customerService.getCustomer(customerId.longValue())));
+	}
+
+	/**
+	 * Gets the customers.
+	 * 
+	 * TODO: Admitir filtros de búsqueda.
+	 * 
+	 * TODO: Paginar
+	 * 
+	 * TODO: Ordenar
+	 * 
+	 * @return the customers
+	 */
+	@Override
+	public ResponseEntity<List<ar.com.plug.generated.model.Customer>> getCustomers() {
+
+		List<ar.com.plug.generated.model.Customer> customerList = customerService.getCustomers().stream()
+				.map((customerEntity) -> dtoMapper.from(customerEntity)).collect(Collectors.toList());
+
+		return ResponseEntity.ok(customerList);
+
+	}
+
 }
