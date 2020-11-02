d6380783ad4d5d2f92cdb32eabed2f3f83050b65
Martín Cortina <micortina@teco.com.ar>
2020-11-02 12:26:39 -0300

    Se agregan métodos a DTOMapper para actualizar una entidad de dominio con otra

--- src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
+++ src/main/java/ar/com/plug/examen/domain/mapper/DTOMapper.java
@@ -1,6 +1,8 @@
 package ar.com.plug.examen.domain.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 import ar.com.plug.examen.domain.model.Customer;
@@ -8,12 +10,12 @@ import ar.com.plug.examen.domain.model.Product;
 import ar.com.plug.examen.domain.model.PurchaseTransaction;
 import ar.com.plug.examen.domain.model.Vendor;
 
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface DTOMapper {
 
 	DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
+	// entity <-> dto conversion
 	public Product from(ar.com.plug.generated.model.Product productDTO);
 
 	public ar.com.plug.generated.model.Product from(Product productEntity);
@@ -29,5 +31,13 @@ public interface DTOMapper {
 	public PurchaseTransaction from(ar.com.plug.generated.model.PurchaseTransaction purchaseTransactionDTO);
 
 	public ar.com.plug.generated.model.PurchaseTransaction from(PurchaseTransaction purchaseTransactionEntity);
+
+	// entity updates
+	
+	void updateVendor(Vendor newEntity, @MappingTarget Vendor oldEntity);
+
+	void updateCustomer(Customer newEntity, @MappingTarget Customer oldEntity);
+
+	void updateProduct(Product newEntity, @MappingTarget Product oldEntity);
 
 }
