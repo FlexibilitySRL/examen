127ae5ca95f6cff0014a19433709cb009ee20ab9
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-11-11 10:54:32 -0300

    added product not exist exception

--- src/main/java/ar/com/plug/examen/domain/exceptions/ProductDoesNotExistException.java
+++ src/main/java/ar/com/plug/examen/domain/exceptions/ProductDoesNotExistException.java
@@ -1,2 +1,7 @@
-package ar.com.plug.examen.domain.exceptions;public class ProductDoesNotExistException {
+package ar.com.plug.examen.domain.exceptions;
+
+public class ProductDoesNotExistException extends Exception{
+    public ProductDoesNotExistException(String message){
+        super(message);
+    }
 }
