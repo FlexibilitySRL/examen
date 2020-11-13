b12f2e9f17b61d27fc28391ccfa6bb4c9fd29e94
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-11-13 11:03:20 -0300

    added architecture test to service layer

--- src/main/java/ar/com/plug/examen/domain/service/impl/ClientServiceImpl.java
+++ src/main/java/ar/com/plug/examen/domain/service/impl/ClientServiceImpl.java
@@ -2,16 +2,13 @@ package ar.com.plug.examen.domain.service.impl;
 
 import ar.com.plug.examen.app.repository.ClientRepository;
 import ar.com.plug.examen.domain.exceptions.ClientDoesNotExistException;
-import ar.com.plug.examen.domain.exceptions.ProductDoesNotExistException;
 import ar.com.plug.examen.domain.model.Client;
-import ar.com.plug.examen.domain.model.Product;
 import ar.com.plug.examen.domain.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
--- src/main/java/ar/com/plug/examen/domain/service/impl/ProcessMessageServiceImpl.java
+++ src/main/java/ar/com/plug/examen/domain/service/impl/ProcessMessageServiceImpl.java
@@ -3,8 +3,10 @@ package ar.com.plug.examen.domain.service.impl;
 import ar.com.plug.examen.domain.model.Message;
 import ar.com.plug.examen.domain.service.ProcessMessageService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProcessMessageServiceImpl implements ProcessMessageService {
 
     @Override
