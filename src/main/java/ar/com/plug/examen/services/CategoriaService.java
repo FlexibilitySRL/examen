06d8d03829c3a50a2c16520113ef857e23cb3ade
tedesco8 <tedesco8@hotmail.com>
2020-12-02 23:27:35 -0300

    creando categoriaService

---
+++ src/main/java/ar/com/plug/examen/services/CategoriaService.java
@@ -0,0 +1,36 @@
+package ar.com.plug.examen.services;
+import ar.com.plug.examen.models.CategoriaModel;
+import ar.com.plug.examen.repositories.CategoriaRepository;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    public ArrayList<CategoriaModel> obtenerCategoria() {
+        return (ArrayList<CategoriaModel>) categoriaRepository.findAll();
+    }
+
+    public CategoriaModel guardarCategoria(CategoriaModel categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Optional<CategoriaModel> obtenerPorId(Long id) {
+        return categoriaRepository.findById(id);
+    }
+
+    public boolean eliminarCategoria(Long id) {
+    try {
+        categoriaRepository.deleteById(id);
+        return true;
+    } catch (Exception e) {
+        return false;
+    }
+    }
+    
+}
