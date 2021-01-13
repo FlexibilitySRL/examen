8e4248f92f10fc1b0c3e6e910421e7cfb4cda780
AnahiAguirre <55332408+AnahiAguirre@users.noreply.github.com>
2021-01-13 14:40:16 -0300

    Update AbmClientes.java

--- src/main/java/ar/com/plug/examen/app/api/AbmClientes.java
+++ src/main/java/ar/com/plug/examen/app/api/AbmClientes.java
@@ -1,1 +1,44 @@
-Prueba
+import java.util.ArrayList;
+
+public class Cliente {
+	private long clinteId;
+	private String nombre;
+	private String apellido;
+	ArrayList <Cliente> listCliente = new ArrayList<Cliente>();
+	
+	public long getClinteId() {
+		return clinteId;
+	}
+	public void setClinteId(long clinteId) {
+		this.clinteId = clinteId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	public void agregarCliente(Cliente e) 
+	{
+		
+		
+		listCliente.add(e);
+		
+		
+	}
+	public void eliminarCliente (Cliente e) {
+		
+		listCliente.remove(e);
+		
+	} 
+	
+}
+
