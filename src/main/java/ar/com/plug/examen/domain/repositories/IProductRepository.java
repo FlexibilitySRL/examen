4c0bd0c338ca1a90149f904fbe1d794bdb0fca1e
nmoreyra <nahuel@cipres.io>
2021-07-16 18:12:13 -0300

    delete unused dependencies

--- src/main/java/ar/com/plug/examen/domain/repositories/IProductRepository.java
+++ src/main/java/ar/com/plug/examen/domain/repositories/IProductRepository.java
@@ -1,8 +1,6 @@
 package ar.com.plug.examen.domain.repositories;
 
 import ar.com.plug.examen.domain.model.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
