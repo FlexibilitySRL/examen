68124e3540d7c970985306adcb5515a4f3b8d4b8
nmoreyra <nahuel@cipres.io>
2021-07-16 17:59:18 -0300

    fix

--- src/main/java/ar/com/plug/examen/domain/repositories/IProductRepository.java
+++ src/main/java/ar/com/plug/examen/domain/repositories/IProductRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
-    Page<Product> findAllByName(String name, Pageable pageable);
 }
