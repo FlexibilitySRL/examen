b22176cf3c16999cb565024f3238ccd6b08859e1
agustin corradini <agustin.corradini@coinme.com>
2022-02-13 18:40:43 -0300

    agustin - H2 initial data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO clients (client_id, first_name, last_name) VALUES
+    (1, 'John', 'Smith'),
+    (2, 'Susan', 'Lauren');
+
+INSERT INTO products (product_id, description, price) VALUES
+    (11, 'product1', '10.00'),
+    (22, 'product2', '50.00');
+
+INSERT INTO vendors (vendor_id, name, ssn) VALUES
+    (1, 'vendor1', '11-11-111'),
+    (2, 'vendor2', '22-22-222');
