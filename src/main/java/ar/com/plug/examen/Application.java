a098edd7bba15121b7033da5221dc49138dbd543
agustin corradini <agustin.corradini@coinme.com>
2022-02-13 18:39:24 -0300

    agustin - added openAPI

--- src/main/java/ar/com/plug/examen/Application.java
+++ src/main/java/ar/com/plug/examen/Application.java
@@ -1,17 +1,19 @@
 package ar.com.plug.examen;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @Configuration
-//@ComponentScan("ar.com.plug")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@OpenAPIDefinition(info = @Info(title = "Payments API", version = "1.0", description = "Payments"))
 public class Application {
 
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
