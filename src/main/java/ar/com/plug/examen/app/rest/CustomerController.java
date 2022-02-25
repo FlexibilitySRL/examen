32fa71606552ed78ca4dd05ae750f807172b7ab0
Pablo Hincapie <pablohincapie@hotmail.com>
2022-02-25 02:17:50 -0500

    Add swagger documentation

--- src/main/java/ar/com/plug/examen/app/rest/CustomerController.java
+++ src/main/java/ar/com/plug/examen/app/rest/CustomerController.java
@@ -1,6 +1,5 @@
 package ar.com.plug.examen.app.rest;
 
-import ar.com.plug.examen.domain.config.SwaggerResponseOk;
 import ar.com.plug.examen.domain.dto.CustomerDTO;
 import ar.com.plug.examen.domain.service.CustomerService;
 import io.swagger.annotations.ApiOperation;
@@ -16,26 +15,36 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @ApiOperation(value = "guarda un registro de un cliente")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se agregan los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la adiccion de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
     @PostMapping(path = "/saveCustomer", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
-
-    @ApiOperation(value = "guarda un registro de un producto")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Se agregan los valores correctamente",
-                    response = SwaggerResponseOk.class),
-            @ApiResponse(code = 400, message = "En la adiccion de los datos",
-                    response = SwaggerResponseOk.class),
-    })
     public void createCustomer(@RequestBody CustomerDTO customerDTO) {
         customerService.createCustomer(customerDTO);
     }
 
+    @ApiOperation(value = "Eliminar un registro de un cliente")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se elimaron los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la elimicación de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
     @DeleteMapping(path = "/removeCustomer/{idCustomer}", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public void deleteCustomer(@PathVariable("idCustomer") Long id) {
         customerService.deleteCustomer(id);
     }
 
+    @ApiOperation(value = "guarda un registro de un cliente")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se agregan los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la edicción de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
     @PostMapping(path = "/editCustomer/{idCustomer}", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
 
--- src/main/java/ar/com/plug/examen/app/rest/ProductController.java
+++ src/main/java/ar/com/plug/examen/app/rest/ProductController.java
@@ -1,36 +1,54 @@
 package ar.com.plug.examen.app.rest;
 
-import ar.com.plug.examen.domain.config.SwaggerResponseOk;
 import ar.com.plug.examen.domain.dto.ProductDTO;
 import ar.com.plug.examen.domain.service.ProductService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
 
 @RestController
-@RequestMapping(path="/product")
+@RequestMapping(path = "/product")
 public class ProductController {
 
     @Autowired
-    private  ProductService productService;
+    private ProductService productService;
 
-    @PostMapping(path = "/saveProduct",  produces = {MediaType.APPLICATION_JSON_VALUE },
+    @ApiOperation(value = "guarda un registro de un producto")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se agregan los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la adiccion de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
+    @PostMapping(path = "/saveProduct", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
 
     public void createProduct(@RequestBody ProductDTO productDTO) {
         productService.createProduct(productDTO);
     }
 
-    @DeleteMapping(path = "/removeProduct/{idProduct}",  produces = {MediaType.APPLICATION_JSON_VALUE },
+    @ApiOperation(value = "Eliminar un registro de un producto")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se elimaron los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la elimicación de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
+    @DeleteMapping(path = "/removeProduct/{idProduct}", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public void deleteProduct(@PathVariable("idProduct") Long id) {
         productService.deleteProduct(id);
     }
 
-    @PostMapping(path = "/editProduct/{idProduct}",  produces = {MediaType.APPLICATION_JSON_VALUE },
+    @ApiOperation(value = "guarda un registro de un producto")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se agregan los valores correctamente"),
+            @ApiResponse(code = 400, message = "Envio incorrecto  de información para la edicción de los datos"),
+            @ApiResponse(code = 500, message = "Ocurrio un error desconocido"),
+    })
+    @PostMapping(path = "/editProduct/{idProduct}", produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = MediaType.APPLICATION_JSON_VALUE)
 
     public void modifyProduct(@PathVariable("idProduct") Long id, @RequestBody ProductDTO productDTO) {
